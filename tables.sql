9186e33374d7cf3611c9e229f65b71165ceaa438
Gabsburg <usovd4@gmail.com>
2016-11-24 00:45:03 +0000

    Create tables.sql for testing

---
+++ tables.sql
@@ -0,0 +1,17 @@
+USE northwind;
+
+Select * from customers;
+
+Select last_name from employees;
+
+Select last_name, first_name
+from employees
+where city = "Seattle";
+
+Select last_name, job_title
+from employees
+where notes is NOT NULL;
+
+SELECT employees.first_name, orders.id 
+FROM employees 
+INNER JOIN orders ON employees.id = orders.employee_id;
